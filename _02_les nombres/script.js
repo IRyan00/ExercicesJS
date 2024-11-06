6d6bc4ec80a451367f2f1167ead14c9af03e0955
RyanEymas <eymas.ryan@gmail.com>
2024-11-06 15:54:16 +0100

    Add files via upload

---
+++ _02_les nombres/script.js
@@ -0,0 +1,34 @@
+// 0/
+let res = 28 + 6;
+
+console.log("Le résultat est : " + res);
+
+// 1/
+let res2 = (6 * 4 + 2) / 2;
+
+console.log("Le résultat est : " + res2);
+
+
+// 2/
+let res3 = (1337 + 256) * 2;
+
+console.log("Le prix final que devra payer Hugo est de : " + res3 + "€");
+
+// 3/
+let res4 = (1337 + 256) * 1.5 + 500;
+
+console.log("Le prix final que devra payer Hugo est de : " + res4 + "€");
+
+// 4/
+let temp = 35 - 23;
+let temp2 = temp / 2;
+let tempF = temp2 * 3
+
+console.log(tempF + " jours restants");
+
+// bogue 0/
+let perc = 0.12 * 2350;
+let res5 = 2350 - perc;
+let res6 = res5 + 1000;
+
+console.log("La levée de fond à récoltée " + res6 + "€");
