02417762da2e3e45c12d5ce519d76d3c0361be36
RyanEymas <eymas.ryan@gmail.com>
2024-11-08 15:11:51 +0100

    Add files via upload

---
+++ _04_variables/script.js
@@ -0,0 +1,30 @@
+// bogue 0/
+
+let nomParc = "Le Royaume Magique";
+
+console.log("Bienvenue au parc d'attractions " + nomParc);
+
+// bogue 1/
+
+let clientsHier = 359;
+let clientsAujourdhui = 374;
+let totalClients = clientsHier + clientsAujourdhui;
+
+console.log(totalClients);
+
+// bogue 2/
+
+let nombreDeCases = 64;
+let cases = 2;
+let superficie = nombreDeCases * cases;
+
+console.log("Superficie du plateau : " + superficie + "m2");
+
+// bogue 3/
+
+let h = 13;
+let m = 37;
+let separateur = "h";
+let horaire = h + separateur + m;
+
+console.log("Bienvenue, il est " + horaire);
